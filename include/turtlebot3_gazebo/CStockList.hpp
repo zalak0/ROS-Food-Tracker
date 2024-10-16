4a836aec3c7d88175b56833041272d7b08a87d9f
Angus McLean <naelcmsugna@gmail.com>
2024-10-16 19:30:18 +1100

    2024-Oct-16 - Angus - Implement stock list for refactored system design

--- include/turtlebot3_gazebo/CStockList.hpp
+++ include/turtlebot3_gazebo/CStockList.hpp
@@ -1,6 +1,9 @@
 #ifndef STOCK_LIST
 #define STOCK_LIST
 
+#include <std_msgs/msg/integer.hpp> 
+#include <std_msgs/msg/string.hpp> 
+
 #include <string>
 #include <unordered_map>
 
@@ -9,43 +12,81 @@
 class CItem;
 
 
+//---Enumerators---------------------------------------------------------------
+
+enum eUpdateType
+{
+    CREATE,
+    DELETE,
+    QUANTITY,
+    RENAME
+};
+
+
 /********************************************************************************
-** CStockList: The stock list is responsible for reading, writing, updating and
-**             deleting data from a text stock file.
+** CStockList: The stock list is responsible for recording scanned ArUco tags,
+**             reading, writing, updating and deleting items from the record.
 ********************************************************************************/
-class CStockList
+class CStockList : public rclcpp::Node
 {
 public:
-    // Creates a new stock file with the default name "stock.txt"
+    // Create stock list which exports to the default file name "stock_<export-time>.txt"
     CStockList();
     
-    // Loads stock from a specified file if it exists, otherwise creates a stock
-    // file with the specified file name and path.
+    // Create stock list which exports to the specified file name "<aFilePath>_<export-time>.txt"
     CStockList( std::string aFilePath );
 
     ~CStockList();
 
-    // Add line for new item if 
-    void CreateItem( int aMarkerId );
+    // Retrieve the quantity of an item by its ArUco marker id.
+    // @param aMarkerId The ArUco marker id of the item to query.
+    // @return The item quantity.
+    int GetQuantity( int aMarkerId );
 
-    // Lookup item by id
-    void RetrieveItem( int aMarkerId );
+    // Rename an item to a specified string by its ArUco marker id.
+    // @param aItemId The ArUco marker id of the item to rename.
+    // @param aName The string to rename the item to.
+    void RenameItem( int aItemId, std::string aName );
 
-    // Update item by id with supplied details (aNewInfo must match aMarkerId)
-    void UpdateItem( int aMarkerId, CItem aNewInfo );
-
-    // Delete item by id
-    void DeleteItem( int aMarkerId );
+    // Exports the current stock to "<mFileName>_<export-time>.txt"
+    void WriteToDisk();
 
 private:
     // Variables
-    std::string mFileName;
-    std::unordered_map< int, CItem > mItems;
+    std::string mFileName;                      // The path which stock export to
+    std::unordered_map< int, CItem > mItems;    // Map from ArUco id to item objects
 
-    // CRUD operations should be separated into private functions where appropriate
+    // ROS topic publishers
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr mPubStockUpdate;
 
-    // Loads items from specified file into member variable
-    void LoadFile( std::string aFilePath ); // Nice to have (no rush to implement)
+    // ROS topic subscribers
+    rclcpp::Subscription<std_msgs::msg::Integer>::SharedPtr mSubScannedMarkerId;
+
+    // ROS subscriber callback updates stock record when new ArUco marker is scanned.
+    void RecordStockCallback( const std_msgs::msg::Integer::SharedPtr msg );
+
+    // Creates a new item instance with the specified parameters.
+    // @param aMarkerId The ArUco marker id of the item.
+    void CreateItem( int aMarkerId );
+
+    // Retrieve pointer to item corresponding to specified ArUco marker id.
+    // @param aMarkerId The ArUco marker id of the item.
+    // @return Pointer to item object or NULL if no object with specified id.
+    CItem* RetrieveItem( int aMarkerId );
+
+    // Update item object corresponding to specified ArUco marker id.
+    // @param aMarkerId The ArUco marker id of the item.
+    // @param aQuantity The new quantity of the item.
+    // @param aName The new name to assign to the item.
+    void UpdateItem( int aMarkerId, int aQuantity, std::string aName );
+
+    // Destruct item object corresponding to specified ArUco marker id.
+    // @param aMarkerId The ArUco marker id of the item.
+    void DeleteItem( int aMarkerId );
+
+    // Initialises variables from stock export file.
+    // @param aFilePath The path of the stock export file.
+    void LoadFile( std::string aFilePath );
 };
 
 #endif
---
+++ src/CStockList.cpp
@@ -0,0 +1,118 @@
+#include "turtlebot3_gazebo/CStockList.hpp"
+
+
+CStockList::CStockList()
+: mFileName( "stock" ),
+  mItems()
+{   
+    auto qos = rclcpp::QoS( rclcpp::KeepLast(10) );
+
+    // Initialise publishers
+    mPubStockUpdate = this->create_publisher<std_msgs::msg::String>( "stock_update", qos );
+
+    // Initialise subscribers
+    mScannedMarkerId = this->create_subscription<std_msgs::msg::String>(
+        "scanned_marker_id", qos, std::bind( &CStockList::RecordStockCallback, this, std::placeholders::_1 ));
+}
+
+
+CStockList::CStockList( std::string aFilePath )
+: mFileName( aFilePath ),
+  mItems()
+{
+    auto qos = rclcpp::QoS( rclcpp::KeepLast(10) );
+
+    // Initialise publishers
+    mPubStockUpdate = this->create_publisher<std_msgs::msg::String>( "stock_update", qos );
+
+    // Initialise subscribers
+    mScannedMarkerId = this->create_subscription<std_msgs::msg::String>(
+        "scanned_marker_id", qos, std::bind( &CStockList::RecordStockCallback, this, std::placeholders::_1 ));
+}
+
+
+~CStockList::CStockList()
+{
+    for (std::unordered_map< int, CItem* >::iterator it = mItems.begin(); it != mItems.end(); ++it) {
+        delete it->second;
+    }
+
+    mItems.clear();
+}
+
+
+int CStockList::GetQuantity( int aMarkerId )
+{
+    return mItems[ aMarkerId ].GetQuantity();
+}
+
+
+void CStockList::RenameItem( int aItemId, std::string aName )
+{
+    mItems[aMarkerId].SetName( aName );
+}
+
+
+void CStockList::WriteToDisk()
+{
+    return;
+}
+
+
+void CStockList::RecordStockCallback( const std_msgs::msg::Integer::SharedPtr msg )
+{   
+    item = RetrieveItem( msg->data );
+
+    if( item )
+    {
+        item.IncrementQuantity();
+    }
+    else
+    {
+        CreateItem( msg.data );
+    }
+}
+
+
+void CStockList::CreateItem( int aMarkerId )
+{
+    mItems[ aMarkerId ] = new CItem( aMarkerId );
+}
+
+
+CItem* CStockList::RetrieveItem( int aMarkerId )
+{
+    CItem item = mItems.find( msg->data );
+
+    if( item != mItems.end() )
+    {
+        return item;
+    }
+    else
+    {
+        return null;
+    }
+}
+
+
+void CStockList::UpdateItem( int aMarkerId, int aQuantity, std::string aName )
+{   
+    if( RetrieveItem( aMarkerId ))
+    {   
+        mItems[ aMarkerId ].SetName( aName );
+        mItems[ aMarkerId ].SetQuantity( aQuantity );
+    }
+}
+
+
+void CStockList::DeleteItem( int aMarkerId )
+{
+    delete RetrieveItem( aMarkerId );
+    mItems.erase( aMarkerId );
+}
+
+
+void CStockList::LoadFile( std::string aFilePath )
+{
+    return;
+}
